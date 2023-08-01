ed7678b9f7c62e6e5aeb36e30e325b0aff35eb6a
legam <louisdufourd@gmail.com>
2023-08-01 14:56:15 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    private String username = "admin";
+    private String password = "admin";
+
+    public static void main(String[] args) {
+        String inputUsername;
+        String inputPassword;
+
+
+    }
+}
