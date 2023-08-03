898b3acb420fe99a330d3f97d16df02d9e3cc314
legam <louisdufourd@gmail.com>
2023-08-03 14:52:22 +0200

    tableau

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,7 @@
 import clavier.In;
 
 public class Main {
-
     public static void main(String[] args) {
-
-        int valeur = 0;
-        while (valeur != 1 && valeur != 2 && valeur != 3) {
-            System.out.println(
-                    "1 -> Maison\n" +
-                            "2 -> Battaille\n" +
-                            "3 -> Plag > Jesterio²"
-            );
-            valeur = In.readInteger();
-            switch (valeur) {
-                case 1:
-                    System.out.println("Maison");
-                    break;
-                case 2:
-                    System.out.println("Battaille");
-                    break;
-                case 3:
-                    System.out.println("ntm");
-                    break;
-            }
-        }
+        int[] ints = {0,2,5,7,6,12}
     }
 }
