9f751a908c01b4c89420a4f87d9c8fedd3516de0
legam <louisdufourd@gmail.com>
2023-08-03 12:54:38 +0200

    menu

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,22 @@ public class Main {
     private static final String PASSWORD = "admin";
 
     public static void main(String[] args) {
-        System.out.println("Entrez votre nom d'utilisateur");
-        String userName = In.readString();
-        System.out.println("Entrez votre mots de passe");
-        String passWord = In.readString();
-        if(userName.equals(USERNAME) && passWord.equals(PASSWORD)) {
-            System.out.println("Connexion");
-        } else {
-            System.out.println("Ce login est incorrect");
-        }
+        System.out.println(
+                "m -> Bonjour" +
+                "\np -> maison" +
+                "\nw -> pirate"
+        );
+         String lettre = In.readString();
+         switch (lettre) {
+             case "m":
+                 System.out.println("Bonjour");
+                 break;
+             case "p":
+                 System.out.println("maison");
+                 break;
+             case "w":
+                 System.out.println("pirate");
+                 break;
+         }
     }
 }
