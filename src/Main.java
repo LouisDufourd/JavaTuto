05a9b4929277580e1c09ff629cabcbc30865e006
legam <louisdufourd@gmail.com>
2023-08-03 12:22:55 +0200

    jesterio code

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,22 @@
+import clavier.In;
+
 public class Main {
-    private String username = "admin";
-    private String password = "admin";
+    private static final String USERNAME = "admin";
+    private static final String PASSWORD = "admin";
 
     public static void main(String[] args) {
-        String inputUsername;
-        String inputPassword;
-
-
+        System.out.println("Entrez votre nom d'utilisateur");
+        String userName = In.readString();
+        if (userName.equals(USERNAME)) {
+            System.out.println("Entrez votre mots de passe");
+            String passWord = In.readString();
+            if (passWord.equals(PASSWORD)) {
+                System.out.println("Connexion");
+            } else {
+                System.out.println("Mot de passe erroné");
+            }
+        } else {
+            System.out.println("Cet utilisateur n'existe pas");
+        }
     }
 }
