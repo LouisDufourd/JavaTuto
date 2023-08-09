446d00871b10f2ef3aac229b7093bc40f1bf2bf2
legam <louisdufourd@gmail.com>
2023-08-09 20:19:36 +0200

    tableau

---
+++ src/Eleve.java
@@ -0,0 +1,44 @@
+public class Eleve {
+    private String nom;
+    private String prenom;
+    private double note;
+
+
+    /**
+     * @param nom     Le nom de l'élève
+     * @param prenom: Le prénom de l'élève
+     * @param note:   La note de l'élève
+     * @return        Retourne l'élève
+     */
+    public Eleve(String nom, String prenom, double note) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.note = note;
+    }
+
+    /**
+     * @return Le nom de l'élève
+     */
+    public String getNom() {
+        return nom;
+    }
+
+    /**
+     * @return Le prénom de l'élève
+     */
+    public String getPrenom() {
+        return prenom;
+    }
+
+    /**
+     * @return La note de l'élève
+     */
+    public double getNote() {
+        return note;
+    }
+
+    @Override
+    public String toString() {
+        return "Nom : " + this.nom + "\nPrénom : " + this.prenom + "\nNote : " + this.note;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,50 @@
 import clavier.In;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        int[] ints = {0,2,5,7,6,12}
+        ArrayList<Eleve> eleves = new ArrayList<>();
+        System.out.println(eleves.size());
+        double moy;
+        Eleve eleve;
+        double sum = 0.0;
+        while(true) {
+            System.out.println("Entrée le nom de l'élève");
+            String nom = In.readString();
+            System.out.println("Entrée le prénom de l'élève");
+            String prenom = In.readString();
+            if(nom.equalsIgnoreCase("end") || prenom.equalsIgnoreCase("end")) {
+                break;
+            } else {
+                try {
+                    System.out.println("Entrée la note de l'élève");
+                    double note = In.readDouble();
+                    sum = sum + note;
+                    eleves.add(new Eleve(nom,prenom,note));
+                }
+                catch(Exception e) {
+                    System.out.println("Veuillez entrer une note.");
+                    break;
+                }
+            }
+        }
+        System.out.println("-----------------Affichage du tableau---------------------");
+        printTab(eleves);
+        System.out.println("-----------------Fin Affichage du tableau---------------------");
+        moy = sum / eleves.size();
+        System.out.println("Taille du tableau " + eleves.size());
+        System.out.println("Moyenne : " + moy);
+    }
+
+    public static void printTab(@NotNull ArrayList<Eleve> eleves) {
+        for (int i = 0; i < eleves.size(); i++) {
+            System.out.println(eleves.get(i));
+            if(!((i + 1) >= eleves.size())) {
+                System.out.println();
+            }
+        }
     }
 }
